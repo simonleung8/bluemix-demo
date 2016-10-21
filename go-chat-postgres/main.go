b6be7a402c26032e85408845bf53cff532d2cddd
Simon Leung <leungs@us.ibm.com>
2016-10-21 10:45:35 +0800

    a little delay for log to start streaming

--- go-chat-postgres/main.go
+++ go-chat-postgres/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/simonleung8/bluemix-demo/go-chat-postgres/db"
 	"github.com/simonleung8/bluemix-demo/go-chat-postgres/server"
 )
 
 func main() {
+	//sleep a little for logs to start streaming
+	time.Sleep(5 * time.Second)
 	databaseClient, err := db.NewClient()
 	if err != nil {
 		log.Fatal(err)
